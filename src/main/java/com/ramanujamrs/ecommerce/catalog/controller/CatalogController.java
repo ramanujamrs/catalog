1f9752ae0c035f02752f5dbab1b2694a6df849e6
Ramanujam Srinivasan <ramanujam.rs@gmail.com>
2020-04-24 11:50:04 -0500

    Added cj mysql Driver Added catalog/all endpoint and dummy data

--- src/main/java/com/ramanujamrs/ecommerce/catalog/controller/CatalogController.java
+++ src/main/java/com/ramanujamrs/ecommerce/catalog/controller/CatalogController.java
@@ -5,10 +5,28 @@
 
 package com.ramanujamrs.ecommerce.catalog.controller;
 
+import com.ramanujamrs.ecommerce.catalog.model.Catalog;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/v1/catalog")
+@RequestMapping(value = "/api/v1/catalog")
 public class CatalogController {
+
+    @GetMapping("/all")
+    public List<Catalog> getCatalog()
+    {
+        List<Catalog> catalogList = new ArrayList<>();
+
+        Timestamp dummytimestamp = new Timestamp(System.currentTimeMillis());
+        Catalog cat1 = new Catalog(1L, "Back to School", "Back to School specific Catalog", "Y", dummytimestamp, dummytimestamp);
+        catalogList.add(cat1);
+
+        return catalogList;
+    }
 }
